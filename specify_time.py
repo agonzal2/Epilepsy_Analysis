96748fe7fe4a3a0a6042c8bf97e782513809ad5f
agonzal2 <agonzal2@staffmail.ed.ac.uk>
2017-11-23 15:00:29 +0000

    Delete specify_time.py

--- specify_time.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 22 16:16:53 2017
-
-@author: Alfredo Gonzalez-Sulser, University of Edinburgh
-email: agonzal2@staffmail.ed.ac.uk
-"""
-from matplotlib.pyplot import *
-from numpy import *
